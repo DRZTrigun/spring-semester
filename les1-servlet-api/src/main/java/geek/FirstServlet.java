ab91c43f02bcdb1378e54032cfdaecb35da22365
Александр <trigun1000@mail.ru>
2021-01-28 23:09:15 +0300

    Initial commit

---
+++ les1-servlet-api/src/main/java/geek/FirstServlet.java
@@ -0,0 +1,35 @@
+package geek;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/first-servlet")
+public class FirstServlet implements Servlet {
+
+    private ServletConfig servletConfig;
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        this.servletConfig = servletConfig;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return this.servletConfig;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.getWriter().println("<h1>Hello from servlet!!! и русский язык</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
