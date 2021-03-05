c20c84729ad7a6dddedd6c0e167fe99bf69ef072
Александр <trigun1000@mail.ru>
2021-03-05 19:41:42 +0300

    lesson11

--- les1-servlet-api/src/main/webapp/users.jsp
+++ les1-servlet-api/src/main/webapp/users.jsp
@@ -1,4 +1,4 @@
-<%@ page import="geek.persist.User" %>
+<%@ page import="geek.persist.persistUser.User" %>
 <%@ page import="java.util.List" %>
 
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
--- les7And4-spring/src/main/java/geek/persist/store/ProductSpecification.java
+++ les7And4-spring/src/main/java/geek/persist/store/ProductSpecification.java
@@ -1,6 +1,5 @@
-package geek.store;
+package geek.persist.store;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.math.BigDecimal;
@@ -8,14 +7,14 @@ import java.math.BigDecimal;
 public final class ProductSpecification {
 
     public static Specification<Product> titleLike(String titleProduct) {
-        return ((root, query, cb) -> cb.like(root.get("title"), "%" + titleProduct + "%"));
+        return (root, query, builder) -> builder.like(root.get("title"), "%" + titleProduct + "%");
     }
 
     public static Specification<Product> minPrice(BigDecimal minPrice){
-        return ((root, query, cb) -> cb.ge(root.get("price"), minPrice));
+        return (root, query, builder) -> builder.greaterThanOrEqualTo(root.get("price"), minPrice);
     }
 
     public static Specification<Product> maxPrice(BigDecimal maxPrice){
-        return ((root, query, cb) -> cb.le(root.get("price"), maxPrice));
+        return (root, query, builder) -> builder.lessThanOrEqualTo(root.get("price"), maxPrice);
     }
 }
